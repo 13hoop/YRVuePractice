b56b24f5b54586a889884b37d9294a64cb372691
YongRen <zhanxin13@gmail.com>
2017-05-26 21:14:41 +0800

    todoDemo basic

--- app.js
+++ app.js
@@ -3,6 +3,31 @@ import Vue from 'vue';
 var app = new Vue({
   el: '#app',
   data: {
-    newTodo: "Hello Vue! It's auto reload? 单纯使用webpack-server默认并不会去走output的"
+    newTodo: '',
+    todoList: []
+  },
+  methods: {
+    addTodo: function () {
+      this.todoList.push({
+        title: this.newTodo,
+        createdAt: new Date(),
+        done: false
+      })
+      console.log(this.todoList)
+      this.newTodo = ''
+    },
+    remove: function(todo) {
+      let index = this.todoList.indexOf(todo)
+      this.todoList.splice(index, 1)
+    }
+  },
+  created: function() {
+    window.onbeforeunload = () => {
+      let dataStr = JSON.stringify(this.todoList)
+      window.localStorage.setItem('todoList', dataStr)
+    }
+    let oldDataStr = window.localStorage.getItem('todoList')
+    let oldData = JSON.parse(oldDataStr)
+    this.todoList = oldData || []
   }
 })
